5496072711900bdc2a3c926ca058bfd6eb827024
Christopher Truncer <chris@christophertruncer.com>
2014-02-26 13:07:00 -0500

    Adding checks for setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# Check to make sure you are root!
+# Thanks to @themightyshiv for helping to get a decent setup script out
+uid=`id -u`
+if [ "${uid}" != '0' ]; then
+  echo ' [Error]: You must run this setup with root privileges.'
+  exit 1
+fi
+
 apt-get install python-pip
 pip install Ghost.py
