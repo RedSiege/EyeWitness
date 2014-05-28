023ad1471501b6bf0e85d009b3d24ed6f04193e1
Christopher Truncer <chris@christophertruncer.com>
2014-05-27 21:43:39 -0400

    Still working on css file, and learning ruby in general

--- EyeWitness.rb
+++ EyeWitness.rb
@@ -5,8 +5,9 @@
 require 'selenium-webdriver'
 
 
-def folder_out()
+def folder_out(dir_name, full_path, local_os)
 
+    # Create the CSS file for the report, and remove the extra 4 spaces
     css_file = 'img {
     max-width: 100%;
     height: auto;
@@ -17,14 +18,31 @@ def folder_out()
     max-height: 550px;
     }'.gsub('    ', '')
 
+    # Write the css file out
     File.open('style.css', 'w') do |stylesheet|
         stylesheet.puts css_file
     end
 
-    date_time = Time.new
-    date = "#{date_time.month}#{date_time.day}#{date_time.year}"
-    hours = "#{date_time.hour}#{date_time.min}#{date_time.sec}"
-    output_folder_name = "#{date}_#{hours}"
+    # Check to see if the directory name is null or not
+    if dir_name.nil?
+        # Get the Time and Date for creating the folder structure
+        date_time = Time.new
+        date = "#{date_time.month}#{date_time.day}#{date_time.year}"
+        hours = "#{date_time.hour}#{date_time.min}#{date_time.sec}"
+        output_folder_name = "#{date}_#{hours}"
+    else
+        output_folder_name = dir_name
+    end
+
+    # Create the paths for making the directories (valid for Win and nix)
+    output_folder_name = File.join(output_folder_name)
+    source_out_folder_name = File.join("#{output_folder_name}", "source")
+    screen_out_folder_name = File.join("#{output_folder_name}", "screen")
+
+    # Actually create the directories now
+    Dir.mkdir(output_folder_name)
+    Dir.mkdir("#{output_folder_name}/source")
+    Dir.mkdir("#{output_folder_name}/screens")
 
     return
 end
