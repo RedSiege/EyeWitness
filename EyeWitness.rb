339d3171367128121f79ab4d4a857c80bff22180
Christopher Truncer <chris@christophertruncer.com>
2014-05-28 08:40:32 -0400

    Finished folder_out function

--- EyeWitness.rb
+++ EyeWitness.rb
@@ -18,33 +18,41 @@ def folder_out(dir_name, full_path, local_os)
     max-height: 550px;
     }'.gsub('    ', '')
 
-    # Write the css file out
-    File.open('style.css', 'w') do |stylesheet|
-        stylesheet.puts css_file
-    end
-
     # Check to see if the directory name is null or not
     if dir_name.nil?
         # Get the Time and Date for creating the folder structure
         date_time = Time.new
-        date = "#{date_time.month}#{date_time.day}#{date_time.year}"
-        hours = "#{date_time.hour}#{date_time.min}#{date_time.sec}"
+        current_date = "#{date_time.month}#{date_time.day}#{date_time.year}"
+        current_time = "#{date_time.hour}#{date_time.min}#{date_time.sec}"
         output_folder_name = "#{date}_#{hours}"
     else
         output_folder_name = dir_name
     end
 
-    # Create the paths for making the directories (valid for Win and nix)
     output_folder_name = File.join(output_folder_name)
-    source_out_folder_name = File.join("#{output_folder_name}", "source")
-    screen_out_folder_name = File.join("#{output_folder_name}", "screen")
+
+    # Check to see if output folder starts with C:\ or /
+    if (output_folder_name.start_with?("C:\\") or output_folder_name.start_with?("/"))
+        # Create the paths for making the directories (valid for Win and nix)
+        source_out_folder_name = File.join("#{output_folder_name}", "source")
+        screen_out_folder_name = File.join("#{output_folder_name}", "screen")
+    else
+        output_folder_name = File.join(full_path, output_folder_name)
+        source_out_folder_name = File.join("#{output_folder_name}", "source")
+        screen_out_folder_name = File.join("#{output_folder_name}", "screen")
+    end
 
     # Actually create the directories now
     Dir.mkdir(output_folder_name)
-    Dir.mkdir("#{output_folder_name}/source")
-    Dir.mkdir("#{output_folder_name}/screens")
+    Dir.mkdir(source_out_folder_name)
+    Dir.mkdir(screen_out_folder_name)
 
-    return
+    # Write the css file out
+    File.open('#{output_folder_name}/style.css', 'w') do |stylesheet|
+        stylesheet.puts css_file
+    end
+
+    return output_folder_name, current_date, current_time
 end
 
 
