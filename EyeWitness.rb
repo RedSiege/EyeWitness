4ddbeeca2e1e079689022eb59296a94f8d1eb8ca
Christopher Truncer <chris@christophertruncer.com>
2014-06-13 22:38:34 -0400

    Added nessus parser

--- EyeWitness.rb
+++ EyeWitness.rb
@@ -104,6 +104,106 @@ class CliParser
 end # End cli_parser class
 
 
+class NessusParser < Nokogiri::XML::SAX::Document
+  
+  def initialize
+    @system_name = nil
+    @port_number = nil
+    @service_name = nil
+    @plugin_name = nil
+    @get_text = false
+    @web_services = ['www', 'http?', 'https?']
+    @url_list = []
+  end
+
+  def start_element name, attrs = []
+    @attrs = attrs
+
+    # Get the IP or name of the system scanned
+    if name == "ReportHost"
+      @attrs.each do |key, value|
+        if key == "name"
+          @system_name = value
+        end
+      end
+    end
+
+    # Grab the port number, service name, and plugin name
+    if name == "ReportItem"
+      @attrs.each do |key, value|
+        if key == "port"
+          @port_number = value
+        end
+
+        if key == "svc_name"
+          @service_name = value
+        end
+
+        if key == "pluginName"
+          value = value.downcase
+          @web_services.each do |web_svc|
+            if (@service_name.include? web_svc and value.include? "service detection")
+              @plugin_name = value
+            else
+              @get_plug_out = false
+            end
+          end
+        end
+      end   # End of Report Items iterator
+    end   # End of Report Item If statement
+
+    if name == "plugin_output"
+      if !@plugin_name.nil?
+        @get_text = true
+      end
+    end
+  end    # End of start_element function
+
+  def characters string
+    if @get_text and !string.empty?
+      @plugin_output = string.gsub('\n', '')
+      @get_text = false
+    end
+  end   # End of characters function
+
+
+  def end_element name
+
+    if (name == "plugin_output" and !@plugin_output.nil?)
+      if (@plugin_output.include? 'TLS' or @plugin_output.include? 'SSL')
+        @final_url = "https://#{@system_name}:#{@port_number}"
+        if !@url_list.include? @final_url
+          @url_list << @final_url
+        end
+      else
+        @final_url = "http://#{@system_name}:#{@port_number}"
+        if !@url_list.include? @final_url
+          @url_list << @final_url
+        end
+      end
+    end
+
+    if name == "ReportItem"
+      @plugin_output = nil
+      @port_number = nil
+      @service_name = nil
+      @plugin_name = nil
+      @get_text = false
+    end
+
+    if name == "ReportHost"
+      @system_name = nil
+    end
+  end   # End of end_element function
+
+
+  def url_get
+    @url_list
+  end
+
+end   # End of nmap parsing class
+
+
 class NmapParser < Nokogiri::XML::SAX::Document
   
   def initialize
