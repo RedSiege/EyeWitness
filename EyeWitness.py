a84ef62ab7b38fb7ee4d1cdc73c1d3739124c2d7
Rohan Vazarkar <y2kspy@gmail.com>
2014-12-16 15:36:44 -0500

    Single Screenshot + UA Selenium

--- EyeWitness.py
+++ EyeWitness.py
@@ -306,11 +306,14 @@ def checkHostPort(ip_to_check, port_to_check):
     return result
 
 
-def createSeleniumDriver(cli_parsed):
+def createSeleniumDriver(cli_parsed, user_agent=None):
     profile = webdriver.FirefoxProfile()
 
     if cli_parsed.useragent is not None:
         profile.set_preference('general.useragent.override', cli_parsed.useragent)
+
+    if user_agent is not None:
+        profile.set_preference('general.useragent.override', user_agent)
 
     if cli_parsed.proxy_ip is not None and cli_parsed.proxy_port is not None:
         profile.set_preference('network.proxy.type', 1)
@@ -320,6 +323,7 @@ def createSeleniumDriver(cli_parsed):
         profile.set_preference('network.proxy.ssl_port', cli_parsed.proxy_port)
 
     driver = webdriver.Firefox(profile)
+    #driver.maximize_window()
     driver.set_page_load_timeout(cli_parsed.t)
     return driver
 
@@ -1137,7 +1141,7 @@ def screenshot_vnc(width, height, vnc_hosts, output_obj, vnc_report,
     return vnc_object, vnc_report, total_pages_vnc
 
 
-def selenium_capture(selenium_object, requesting_object, screen_name, output_obj):
+def selenium_capture(selenium_object, requesting_object, screen_name, output_obj, source_code_name):
     selenium_object.get(requesting_object.return_remote_system_address())
 
     if output_obj.report_folder.startswith("/") or re.match('[A-Za-z]:',output_obj.report_folder):
@@ -1147,11 +1151,23 @@ def selenium_capture(selenium_object, requesting_object, screen_name, output_obj
     else:
         capture_path = join(output_obj.report_folder, "screens", screen_name)
 
+    if output_obj.report_folder.startswith("/") or output_obj.report_folder.startswith("C:\\"):
+        report_file = join(output_obj.report_folder, "source", source_code_name)
+    else:
+        report_file = join(output_obj.eyewitness_path, output_obj.report_folder, "source",
+                           source_code_name)
+
     headers = dict(urllib2.urlopen(requesting_object.return_remote_system_address()).info())
 
     selenium_object.save_screenshot(capture_path)
-    requesting_object.set_web_response_attributes(selenium_object.page_source, headers, capture_path)
+    requesting_object.set_web_response_attributes(selenium_object.page_source.encode('utf-8'), headers, capture_path)
     
+    with open(report_file, 'w') as source:
+        source.write(requesting_object.web_source_code)
+
+    requesting_object.set_default_creds(default_creds(
+        requesting_object.web_source_code, output_obj.eyewitness_path))
+
     return requesting_object
 
 
@@ -2502,11 +2518,8 @@ if __name__ == "__main__":
 
         # If the user wants to cycle through user agents, return the
         # disctionary of applicable user agents
-        if cli_parsed.cycle is None:
-            selenium_object = createSeleniumDriver(cli_parsed)
-        else:
+        if cli_parsed.cycle is not None:
             ua_dict = user_agent_definition(cli_parsed.cycle)
-            selenium_object = createSeleniumDriver(cli_parsed)
 
         # Logging setup
         logging.basicConfig(filename=log_file_path, level=logging.WARNING)
@@ -2539,9 +2552,10 @@ if __name__ == "__main__":
 
             # If a normal single request, then perform the request
             if cli_parsed.cycle is None:
+                selenium_object = createSeleniumDriver(cli_parsed)
 
                 try:
-                    web_request_object = selenium_capture(selenium_object, web_request_object, picture_name, ew_output_object)
+                    web_request_object = selenium_capture(selenium_object, web_request_object, picture_name, ew_output_object, source_name)
                     selenium_object.close()
                     # Create the table info for the single URL (screenshot,
                     # server headers, etc.)
@@ -2586,6 +2600,44 @@ if __name__ == "__main__":
                 source_name = source_name + "_baseline.txt"
                 picture_name = picture_name + "_baseline.png"
                 request_number = 0
+                content_blank = 0
+
+                try:
+                    print "Making baseline request..."
+                    selenium_object = createSeleniumDriver(cli_parsed)
+                    web_request_object = selenium_capture(selenium_object, web_request_object, picture_name, ew_output_object, original_source)
+                    selenium_object.close()
+                    # Create the table info for the single URL (screenshot,
+                    # server headers, etc.)
+                    web_index = table_maker(
+                        web_request_object, web_index,
+                        content_blank,
+                        log_file_path, "Baseline (n/a)", "Baseline (n/a)",
+                        source_name, picture_name, baseline_request,
+                        ew_output_object)
+
+                # Skip a url if Ctrl-C is hit
+                except KeyboardInterrupt:
+                    print "[*] Skipping: " + cli_parsed.single
+                    web_index += """<tr>
+                    <td><a href=\"{single_given_url}\">{single_given_url}</a></td>
+                    <td>User Skipped this URL</td>
+                    </tr>
+                    """.format(single_given_url=cli_parsed.single).replace('    ', '')
+                # Catch timeout warning
+                except TimeoutException:
+                    print "[*] Hit timeout limit when connecting to: " + cli_parsed.single
+                    web_index += """<tr>
+                    <td><a href=\"{single_timeout_url}\" target=\"_blank\">\
+                    {single_timeout_url}</a></td>
+                    <td>Hit timeout limit while attempting screenshot</td>
+                    </tr>
+                    """.format(single_timeout_url=cli_parsed.single)
+
+
+                # Set up sleep if requested
+                jitter_wit_it(cli_parsed)
+
 
                 # Iterate through the user agents the user has selected to use,
                 # and set ghost to use them. Then perform a comparison of the
@@ -2596,99 +2648,48 @@ if __name__ == "__main__":
                     # Create the counter to ensure our file names are unique
                     source_name = original_source + "_" + browser_key + ".txt"
                     picture_name = original_screenshot + "_" + browser_key + ".png"
+                    baseline_length = 0
+                    content_blank = 0
 
                     # Setting the new user agent
-                    ghost_object.page.setUserAgent(user_agent_value)
+                    selenium_object = createSeleniumDriver(cli_parsed,user_agent_value)
 
                     # Making the request with the new user agent
                     print "[*] Now making web request with: " + browser_key
                     try:
-                        if request_number == 0:
-                            # Get baseline screenshot
-                            web_request_object = ghost_capture(
-                                ghost_object, web_request_object,
-                                picture_name, ew_output_object)
+                        new_web_request_object =\
+                            request_object.RequestObject()
 
-                            # Hack for a bug in Ghost at the moment
-                            #baseline_page.content = "None"
+                        # Set the web request info for the request object
+                        new_web_request_object.set_web_request_attributes(
+                            cli_parsed.single)
 
-                            baseline_content_blank = backup_request(
-                                web_request_object, source_name,
-                                content_blank, ew_output_object)
-                            extra_info = "This is the baseline request"
+                        new_web_request_object = selenium_capture(
+                            selenium_object, new_web_request_object,
+                            picture_name, ew_output_object, source_name)
+                        selenium_object.close()
 
+                        # Function which hashes the original request
+                        # with the new request and checks to see if
+                        # they are identical
+                        same_or_different, total_length_difference = \
+                            request_comparison(
+                                web_request_object.web_source_code,
+                                new_web_request_object.web_source_code,
+                                cli_parsed.difference)
+
+                        if same_or_different:
+                            pass
+                        else:
                             # Create the table info for the single URL
                             # (screenshot, server headers, etc.)
                             web_index = table_maker(
-                                web_request_object, web_index,
-                                baseline_content_blank,
-                                log_file_path, browser_key, user_agent_value,
-                                source_name, picture_name, baseline_request,
+                                new_web_request_object, web_index,
+                                content_blank,
+                                log_file_path, browser_key,
+                                user_agent_value, source_name,
+                                picture_name, total_length_difference,
                                 ew_output_object)
-
-                            # Move beyond the baseline
-                            request_number = 1
-
-                        else:
-
-                            # Create the request object that will be passed
-                            new_web_request_object =\
-                                request_object.RequestObject()
-
-                            # Set the web request info for the request object
-                            new_web_request_object.set_web_request_attributes(
-                                cli_parsed.single)
-
-                            new_web_request_object = ghost_capture(
-                                ghost_object, new_web_request_object,
-                                picture_name, ew_output_object)
-
-                            try:
-                                # Hack for a bug in Ghost at the moment
-                                # new_ua_page.content = "None"
-
-                                new_ua_content_blank = backup_request(
-                                    new_web_request_object, source_name,
-                                    content_blank, ew_output_object)
-
-                                # Function which hashes the original request
-                                # with the new request and checks to see if
-                                # they are identical
-                                same_or_different, total_length_difference = \
-                                    request_comparison(
-                                        web_request_object.web_source_code,
-                                        new_web_request_object.web_source_code,
-                                        cli_parsed.difference)
-
-                                # If they are the same, then go on to the next
-                                # user agent, if they are different, add it to
-                                # the report
-                                if same_or_different:
-                                    pass
-                                else:
-                                    # Create the table info for the single URL
-                                    # (screenshot, server headers, etc.)
-                                    web_index = table_maker(
-                                        new_web_request_object, web_index,
-                                        content_blank,
-                                        log_file_path, browser_key,
-                                        user_agent_value, source_name,
-                                        picture_name, total_length_difference,
-                                        ew_output_object)
-
-                            except AttributeError:
-                                print "[*] Unable to request " + cli_parsed.single +\
-                                    " with " + browser_key
-                                web_index += """<tr>
-                                <td><a href=\"{single_given_url}\">\
-                                {single_given_url}</a></td>
-                                <td>Unable to request {single_given_url} with \
-                                {browser_user}.</td>
-                                </tr>
-                                """.format(single_given_url=cli_parsed.single,
-                                           browser_user=browser_key).\
-                                    replace('    ', '')
-                            total_length_difference = "None"
 
                     # Skip a url if Ctrl-C is hit
                     except KeyboardInterrupt:
@@ -2710,18 +2711,7 @@ if __name__ == "__main__":
                         </tr>
                         """.format(single_timeout_url=cli_parsed.single)
 
-                    # Add Random sleep based off of user provided jitter value
-                    #  if requested
-                    if cli_parsed.jitter is not "None":
-                        sleep_value = random.randint(0, 30)
-                        sleep_value = sleep_value * .01
-                        sleep_value = 1 - sleep_value
-                        sleep_value = sleep_value * int(cli_parsed.jitter)
-                        print "[*] Sleeping for " + str(sleep_value) + " seconds.."
-                        try:
-                            time.sleep(sleep_value)
-                        except KeyboardInterrupt:
-                            print "[*] User cancelled sleep for this URL!"
+                    jitter_wit_it(cli_parsed)
 
             # Write out the report for the single URL
             create_link_structure(
