bc2fa7ad7ce60fa531a769eccc230d60f98a245e
Rohan Vazarkar <y2kspy@gmail.com>
2015-05-08 10:35:43 -0400

    Modularized further

--- new_eyewitness.py
+++ new_eyewitness.py
@@ -11,9 +11,16 @@ import time
 from helpers import get_ua_values
 from helpers import target_creator
 from helpers import title_screen
+from helpers import write_report
+from helpers import create_web_index_head
 from modules import objects
 from modules import phantomjs_module
 from modules import selenium_module
+from fuzzywuzzy import fuzz
+from multiprocessing import Manager
+from multiprocessing import Pool
+from multiprocessing import Process
+from multiprocessing import Queue
 
 
 def create_cli_parser():
@@ -195,6 +202,91 @@ def create_folders_css(cli_parsed):
     with open(os.path.join(cli_parsed.d, 'style.css'), 'w') as f:
         f.write(css_page)
 
+
+def single_mode(cli_parsed, url=None, q=None):
+    if cli_parsed.web:
+        create_driver = selenium_module.create_driver
+        capture_host = selenium_module.capture_host
+    elif cli_parsed.headless:
+        create_driver = phantomjs_module.create_driver
+        capture_host = phantomjs_module.capture_host
+
+    if url is None:
+        url = cli_parsed.single
+    http_object = objects.HTTPTableObject()
+    http_object.remote_system = url
+    http_object.set_paths(
+        cli_parsed.d, 'baseline' if cli_parsed.cycle is not None else None)
+
+    web_index_head = create_web_index_head(cli_parsed.date, cli_parsed.time)
+
+    if cli_parsed.cycle is not None:
+        print 'Making baseline request for {0}'.format(http_object.remote_system)
+    else:
+        print 'Attempting to screenshot {0}'.format(http_object.remote_system)
+    driver = create_driver(cli_parsed)
+    result, driver = capture_host(cli_parsed, http_object, driver)
+    driver.quit()
+    if cli_parsed.cycle is not None and result.error_state is None:
+        ua_dict = get_ua_values(cli_parsed.cycle)
+        for browser_key, user_agent_value in ua_dict.iteritems():
+            print 'Now making web request with: {0} for {1}'.format(
+                browser_key, result.remote_system)
+            ua_object = objects.UAObject(browser_key, user_agent_value)
+            ua_object.copy_data(result)
+            driver = create_driver(cli_parsed, user_agent_value)
+            ua_object, driver = capture_host(cli_parsed, ua_object, driver)
+            result.add_ua_data(ua_object)
+            driver.quit()
+
+    if q is None:
+        html = result.create_table_html()
+        with open(os.path.join(cli_parsed.d, 'report.html'), 'w') as f:
+            f.write(web_index_head)
+            f.write(html)
+    else:
+        q.put(result)
+
+
+def multi_mode(cli_parsed):
+    p = Pool(cli_parsed.threads)
+    m = Manager()
+    data = m.Queue()
+    threads = []
+
+    url_list, rdp_list, vnc_List = target_creator(cli_parsed)
+
+    for url in url_list:
+        threads.append(p.apply_async(single_mode, [cli_parsed, url, data]))
+
+    p.close()
+    try:
+        while not all([r.ready() for r in threads]):
+            time.sleep(1)
+    except KeyboardInterrupt:
+        p.terminate()
+        p.join()
+
+    results = []
+    while not data.empty():
+        results.append(data.get())
+
+    grouped = []
+    errors = [x for x in results if x.error_state is not None]
+    results[:] = [x for x in results if x.error_state is None]
+    while len(results) > 0:
+        test = results.pop(0)
+        temp = [x for x in results if fuzz.token_sort_ratio(
+            test.page_title, x.page_title) >= 70]
+        temp.append(test)
+        temp = sorted(temp, key=lambda (k): k.page_title)
+        grouped.extend(temp)
+        results[:] = [x for x in results if fuzz.token_sort_ratio(
+            test.page_title, x.page_title) < 70]
+    grouped.extend(errors)
+    errors = sorted(errors, key=lambda (k): k.error_state)
+    write_report(grouped, cli_parsed)
+
 if __name__ == "__main__":
     title_screen()
     cli_parsed = create_cli_parser()
@@ -208,16 +300,10 @@ if __name__ == "__main__":
     create_folders_css(cli_parsed)
 
     if cli_parsed.single:
-        if cli_parsed.web:
-            selenium_module.single_mode(cli_parsed)
-        elif cli_parsed.headless:
-            phantomjs_module.single_mode(cli_parsed)
+        single_mode(cli_parsed)
 
     if cli_parsed.f is not None:
-        if cli_parsed.web:
-            selenium_module.multi_mode(cli_parsed)
-        elif cli_parsed.headless:
-            phantomjs_module.multi_mode(cli_parsed)
+        multi_mode(cli_parsed)
 
     print('\n[*] Done! Check out the report in the {0} folder!').format(
         cli_parsed.d)
