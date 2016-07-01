8e46624a15cbb30fff835aa6b53e4830195797da
Chris Truncer <CTruncer@christophertruncer.com>
2016-07-01 15:27:04 -0600

    I think working for ubuntu

--- setup/setup.sh
+++ setup/setup.sh
@@ -138,10 +138,16 @@ case ${osinfo} in
       tar -xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
       cd phantomjs-2.1.1-linux-x86_64/bin/
       mv phantomjs ../../
+      rm -rf phantomjs-2.1.1-linux-x86_64
+      rm phantomjs-2.1.1-linux-x86_64.tar.bz2
     else
-      wget -O phantomjs https://www.christophertruncer.com/InstallMe/phantom_ubu
+      wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
+      tar -xvf phantomjs-2.1.1-linux-i686.tar.bz2
+      cd phantomjs-2.1.1-linux-i686/bin/
+      mv phantomjs ../../
+      rm -rf phantomjs-2.1.1-linux-i686
+      rm phantomjs-2.1.1-linux-i686.tar.bz2
     fi
-    chmod +x phantomjs
     cd ..
   ;;
   # CentOS 6.5+ Dependency Installation
