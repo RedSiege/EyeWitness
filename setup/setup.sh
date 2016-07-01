0a9264eadbfb4514879c39de3226c58218999742
Chris Truncer <CTruncer@christophertruncer.com>
2016-07-01 16:01:29 -0600

    most distros use official binary

--- setup/setup.sh
+++ setup/setup.sh
@@ -106,8 +106,24 @@ case ${osinfo} in
     pip install beautifulsoup4
     echo
     cd ../bin/
-    wget -O phantomjs https://www.christophertruncer.com/InstallMe/phantom_deb
-    chmod +x phantomjs
+    MACHINE_TYPE=`uname -m`
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+      wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+      tar -xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
+      cd phantomjs-2.1.1-linux-x86_64/bin/
+      mv phantomjs ../../
+      cd ../..
+      rm -rf phantomjs-2.1.1-linux-x86_64
+      rm phantomjs-2.1.1-linux-x86_64.tar.bz2
+    else
+      wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
+      tar -xvf phantomjs-2.1.1-linux-i686.tar.bz2
+      cd phantomjs-2.1.1-linux-i686/bin/
+      mv phantomjs ../../
+      cd ../..
+      rm -rf phantomjs-2.1.1-linux-i686
+      rm phantomjs-2.1.1-linux-i686.tar.bz2
+    fi
     cd ..
   ;;
   # Ubuntu (tested in 13.10) Dependency Installation
@@ -182,8 +198,24 @@ case ${osinfo} in
     pip install beautifulsoup4
     echo
     cd ../bin/
-    wget http://www.christophertruncer.com/InstallMe/phantomjs
-    chmod +x phantomjs
+    MACHINE_TYPE=`uname -m`
+    if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+      wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+      tar -xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
+      cd phantomjs-2.1.1-linux-x86_64/bin/
+      mv phantomjs ../../
+      cd ../..
+      rm -rf phantomjs-2.1.1-linux-x86_64
+      rm phantomjs-2.1.1-linux-x86_64.tar.bz2
+    else
+      wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
+      tar -xvf phantomjs-2.1.1-linux-i686.tar.bz2
+      cd phantomjs-2.1.1-linux-i686/bin/
+      mv phantomjs ../../
+      cd ../..
+      rm -rf phantomjs-2.1.1-linux-i686
+      rm phantomjs-2.1.1-linux-i686.tar.bz2
+    fi
     cd ..
   ;;
   # Notify Manual Installation Requirement And Exit
