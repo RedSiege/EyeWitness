b37bd528a39fb940180f6d7140ea2d8fc38a0d60
Lorenzo <lorenzo@laptop>
2017-08-15 12:36:07 +0100

    Removed hard-coded path check for phantomjs

--- EyeWitness.py
+++ EyeWitness.py
@@ -253,7 +253,9 @@ def single_mode(cli_parsed):
             display = Display(visible=0, size=(1920, 1080))
             display.start()
     elif cli_parsed.headless:
-        if not os.path.isfile('./bin/phantomjs'):
+        if not os.path.isfile(
+            os.path.join(os.path.realpath(__file__), 'bin', 'phantomjs')
+        ):
             print(" [*] Error: You are missing your phantomjs binary!")
             print(" [*] Please run the setup script!")
             sys.exit(0)
