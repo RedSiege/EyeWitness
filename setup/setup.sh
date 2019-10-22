99a02a3bdf547e623624283e15482fa25b3684bd
Jonas Carlsson <jonas@jonasonline.se>
2019-10-22 21:43:04 +0200

    Installing x11utils and upgrading pip to avoid warnings at build and runtime

--- setup/setup.sh
+++ setup/setup.sh
@@ -214,7 +214,8 @@ case ${osinfo} in
   Ubuntu)
     apt-get update
     echo '[*] Installing Debian Dependencies'
-    apt-get install -y cmake python3 xvfb python3-pip python-netaddr python3-dev tesseract-ocr firefox
+    apt-get install -y cmake python3 xvfb python3-pip python-netaddr python3-dev tesseract-ocr firefox x11-utils
+    pip3 install --upgrade pip
     echo '[*] Upgrading paramiko'
     python3 -m pip install --upgrade paramiko
     echo
